3ec20ae9a4fdfa84cf320625a1c34b295781a203
Mariagabisxzs <mariagabrielagomes923@gmail.com>
2024-05-23 10:57:00 -0300

    função result imc

--- SRC/Components/Form/index.js
+++ SRC/Components/Form/index.js
@@ -1,23 +1,69 @@
-import React from "react";
-import {View, Text } from "react-native";
-import { TextInput } from "react-native-web";
+import React, {useState} from "react";
+import { View, Text, TextInput, Button } from "react-native";
+import ResultImc from "./ResultImc";
 
-export default function Title(){
+import default function Form(){
+
+    const [height, setHeight] = useState(null)
+    const [weight, setWeight] = useState(null)
+    const [messageImc, setMessageImc] = useState("informe seu peso e sua altura");
+    const [imc, setImc] = useState(null)
+    const [textButton, setTextButton] = useState("IMC")
+
+    function imcCalculator(){
+      return setImc((weight/(height*height)).toFixed(2))
+    }
+
+    function validationImc(){
+      if(weight != null && height != null){
+        imcCalculator()
+        setHeight(null)
+        setWeight(null)
+        setMessageImc(" SEU IMC É = ")
+        setTextButton("CALCULAR NOVAMENTE")
+        return
+      }
+
+      setImc(null)
+      setTextButton("calcular")
+      setMessageImc("preencha peso e altura")
+
+    }
+
+    
+
+
     return(
-     <View>
         <View>
-        <Text>Altura</Text>
-        <TextInput
-        placeholder="Ex.1.69"
-        keyboardType="numeric"
-        />
+          
+          <View>
+            <Text>Altura</Text>
+            <TextInput
+            onChangeText={setHeight}
+            value={height}
+             placeholder="Ex.1.75"
+             keyboardType="numeric"
+            />
 
-     <Text>Peso</Text>
-        <TextInput
-        placeholder="Ex.69.766"
-        keyboardType="numeric"
-        />
+            <Text>Peso</Text>
+            <TextInput
+            onChangeText={setWeight}
+            value={weight}
+             placeholder="Ex. 85.766"
+             keyboardType="numeric"
+            />
+
+            <Button
+            onPress={() => validationImc()}
+            title={textButton}
+            />
+            
+
+          </View>
+<ResultImc
+messageResultImc = {messageImc}
+ResultImc = {imc}
+/>
         </View>
-     </View>
     );
 }
